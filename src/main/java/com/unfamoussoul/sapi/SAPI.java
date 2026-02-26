22d4eb6258d4d83244f8d1c1ea7c233ae1464f41
unfamoussoul <ibudda.dict@gmail.com>
2026-02-26 12:19:02 +0400

    added getLocale and javadoc to SAPI

--- src/main/java/com/unfamoussoul/sapi/SAPI.java
+++ src/main/java/com/unfamoussoul/sapi/SAPI.java
@@ -1,10 +1,16 @@
 package com.unfamoussoul.sapi;
 
+import com.unfamoussoul.sapi.locale.Locale;
 import com.unfamoussoul.sapi.module.ModuleHandler;
 import org.bukkit.command.CommandMap;
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * Главный класс SAPI. Отсюда можно использовать JavaPlugin методы
+ */
 public abstract class SAPI extends JavaPlugin {
+
+    public abstract Locale getLocale();
 
     public abstract CommandMap getCommandMap();
 
