0f7e04f92b8a5fef10abc9387da7ffea4a3fa729
unfamoussoul <ibudda.dict@gmail.com>
2026-02-28 21:20:05 +0400

    1.2.1 Something broke. Idk what is

--- src/main/java/com/unfamoussoul/sapi/module/ModuleContainer.java
+++ src/main/java/com/unfamoussoul/sapi/module/ModuleContainer.java
@@ -1,8 +1,6 @@
 package com.unfamoussoul.sapi.module;
 
-import com.unfamoussoul.sapi.module.loader.ChildFirstClassLoader;
-
 /**
  * Контейнер с информацией о модуле
  */
-public record ModuleContainer(SAPIModule module, ChildFirstClassLoader classLoader) {}
+public record ModuleContainer(SAPIModule module, ClassLoader classLoader) {}
